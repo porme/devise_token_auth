2479ac9d737197742e602b99c355b61854160e3d
k-810 <k810marina@gmail.com>
2019-08-27 22:37:08 +0900

    edit15

--- lib/devise_token_auth/token_factory.rb
+++ lib/devise_token_auth/token_factory.rb
@@ -105,12 +105,12 @@ module DeviseTokenAuth
       #p Token
       p token
       p "token1"
-      qr_code1 = RQRCode::QRCode.new("https://fukurikun.com/confirm_password?confirmation_token=#{token}", :size => 20, :level => :q)
+      qr_code1 = RQRCode::QRCode.new("https://fukurikun.com/confirm_password?confirmation_token=#{token}", :size => 40, :level => :q)
      @qr_base1 = qr_code1.to_img.resize(200, 200).to_data_url
     end
 p token
 p "token2"
-      qr_code2 = RQRCode::QRCode.new("https://fukurikun.com/confirm_password?confirmation_token=#{token}", :size => 20, :level => :q)
+      qr_code2 = RQRCode::QRCode.new("https://fukurikun.com/confirm_password?confirmation_token=#{token}", :size => 40, :level => :q)
      @qr_base2 = qr_code2.to_img.resize(200, 200).to_data_url
       #p qr
     Token = Struct.new(:client, :token, :token_hash, :expiry) do
