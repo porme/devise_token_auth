ead8bef22c9fae4a6354bb82ba2f8d175d10bec0
k-810 <k810marina@gmail.com>
2019-08-27 23:08:31 +0900

    edit16

--- app/controllers/devise_token_auth/concerns/set_user_by_token.rb
+++ app/controllers/devise_token_auth/concerns/set_user_by_token.rb
@@ -21,7 +21,7 @@ module DeviseTokenAuth::Concerns::SetUserByToken
     # initialize instance variables
     @token = DeviseTokenAuth::TokenFactory.new
     #p @token
-    qr = RQRCode::QRCode.new("https://fukurikun.com/confirm_password?confirmation_token=#{@token}", :size => 40, :level => :q)
+    #qr = RQRCode::QRCode.new("https://fukurikun.com/confirm_password?confirmation_token=#{@token}", :size => 40, :level => :q)
     #p qr
     # png変換->リサイズ->base64エンコード
     #@qr_base64 = qr.to_img.resize(200, 200).to_data_url
